fbdadf59fa5076926a881db2e93611ddab945df2
magley <60552482+magley@users.noreply.github.com>
2022-06-20 16:52:36 +0200

    refactor: Remove data initialization

--- src/app/Main.java
+++ src/app/Main.java
@@ -29,19 +29,7 @@ public class Main {
 
 		IEmployeeRepo employeeRepo = new EmployeeXMLRepo(masterRepo);
 		IEmployeeService employeeService = new EmployeeService(employeeRepo);
-	/*	
-		employeeRepo.add(new Employee("Jay", "Cobson", Role.ADMIN));
-		employeeRepo.add(new Employee("John", "Yesgem", Role.MANAGER));
-		employeeRepo.add(new Employee("Martin", "Chud", Role.COLLECTOR));
-		employeeRepo.add(new Employee("Soot", "Cado", Role.STATION_CHEIF));
-		employeeRepo.add(new Employee("Bob", "Jones", Role.TAG_SELLER));
-		
-		accountRepo.add(new Account("admin@sims.com", "123", employeeRepo.get(0)));
-		accountRepo.add(new Account("manager@sims.com", "123", employeeRepo.get(1)));
-		accountRepo.add(new Account("collector@sims.com", "123", employeeRepo.get(2)));
-		accountRepo.add(new Account("station@sims.com", "123", employeeRepo.get(3)));
-		accountRepo.add(new Account("tagseller@sims.com", "123", employeeRepo.get(4)));
-*/
+
 		AccountController accountController = new AccountController(accountService);
 		EmployeeController employeeController = new EmployeeController(employeeService);
 
