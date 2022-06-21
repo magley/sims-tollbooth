e3e6072a3a4db388b1271758eae05a0600f6430f
magley <60552482+magley@users.noreply.github.com>
2022-06-21 10:38:48 +0200

    feat(station): Add domain classes

---
+++ src/core/station/Place.java
@@ -0,0 +1,35 @@
+package core.station;
+
+import core.Entity;
+
+public class Place extends Entity {
+	private String name;
+	private String zipCode;
+	
+	public Place(String name, String zipCode) {
+		super();
+		this.name = name;
+		this.zipCode = zipCode;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getZipCode() {
+		return zipCode;
+	}
+	
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	@Override
+	public String toString() {
+		return "Place [name=" + name + ", zipCode=" + zipCode + "]";
+	}
+}
---
+++ src/core/station/Station.java
@@ -0,0 +1,50 @@
+package core.station;
+
+import core.Entity;
+
+public class Station extends Entity {
+	public enum Type {
+		ENTER,
+		EXIT
+	}
+
+	private String code;
+	private Type type;
+	private Place place;
+	
+	public Station(String code, Type type, Place place) {
+		super();
+		this.code = code;
+		this.type = type;
+		this.place = place;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	public Type getType() {
+		return type;
+	}
+	
+	public void setType(Type type) {
+		this.type = type;
+	}
+	
+	public Place getPlace() {
+		return place;
+	}
+	
+	public void setPlace(Place place) {
+		this.place = place;
+	}
+
+	@Override
+	public String toString() {
+		return "Station [code=" + code + ", type=" + type + ", place=" + place + "]";
+	}
+}
