ed07560a154fe3ca70aa971e59137295138d3d09
filipkontic <80402248+filipkontic@users.noreply.github.com>
2022-06-27 16:43:49 +0200

    feat(core.station): Add getByType to Service
    This is to be used while generating pricelist entries.

--- src/core/station/StationService.java
+++ src/core/station/StationService.java
@@ -1,10 +1,18 @@
 package core.station;
 
+import java.util.List;
+
 import core.common.ServiceAdapter;
+import core.station.Station.Type;
 
 public class StationService extends ServiceAdapter<Station> implements IStationService {
 
 	public StationService(IStationRepo repo) {
 		super(repo);
+	}
+
+	@Override
+	public List<Station> getByType(Type type) {
+		return repo.getAll(s -> s.getType() == type);
 	}
 }
