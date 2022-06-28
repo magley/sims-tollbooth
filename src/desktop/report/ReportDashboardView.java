eeb0a28e716f557797eacc6c8ddb5a13560786fe
Hristina Adamovic <hristinacina@gmail.com>
2022-06-28 18:47:27 +0200

    Implemented chart for reports

--- src/desktop/report/ReportDashboardView.java
+++ src/desktop/report/ReportDashboardView.java
@@ -2,17 +2,30 @@ package desktop.report;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 import core.AppContext;
+import core.common.FieldEmptyException;
 import core.pricelist.entry.PricelistEntry;
 import core.station.Station;
+import core.station.Station.Type;
 import desktop.ITabbedPanel;
 import net.miginfocom.swing.MigLayout;
+import org.knowm.xchart.SwingWrapper;
 
 public class ReportDashboardView extends JPanel implements ITabbedPanel {
 
@@ -20,6 +33,8 @@ public class ReportDashboardView extends JPanel implements ITabbedPanel {
 
 	private AppContext ctx;
 	private Station station;
+	private Date startDate;
+	private Date endDate;
 
 	private JComboBox<Station> cbStation;
 	private JComboBox<PricelistEntry.VehicleCategory> cbVehicleCategory;
@@ -32,7 +47,7 @@ public class ReportDashboardView extends JPanel implements ITabbedPanel {
 	public ReportDashboardView(AppContext ctx) {
 		this(ctx, null);
 	}
-	
+
 	public ReportDashboardView(AppContext ctx, Station station) {
 
 		this.ctx = ctx;
@@ -43,58 +58,50 @@ public class ReportDashboardView extends JPanel implements ITabbedPanel {
 
 	private void initGUI() {
 		setLayout(new MigLayout("", "[grow]", "[][][][]"));
-		//TODO REORDER
-		JLabel lblStation = new JLabel(String.format("Station: %s", station.getCode()));
-		add(lblStation, "cell 0 0");
-		
-		//TODO add option: all stations
-		cbStation = new JComboBox<Station>(ctx.getStationService().getByType(Station.Type.ENTER).toArray(new Station[0]));
-		add(cbStation, "cell 0 1,growx");	
 
-		JLabel lblStartDate = new JLabel("From date:");
-		add(lblStartDate, "flowx,cell 0 2");
-		
-		JLabel lblEndDate = new JLabel("To date:");
-		add(lblEndDate, "flowx,cell 0 2");
+		JLabel lblStation = new JLabel("Station:");
+		add(lblStation, "cell 0 0");
+
+		// TODO add option: all stations
+		cbStation = new JComboBox<Station>(ctx.getStationService().getByType(Type.EXIT).toArray(new Station[0]));
+		add(cbStation, "cell 0 0,growx");
+		if (station != null) {
+			cbStation.setSelectedItem(station);
+			cbStation.setEnabled(false);
+		}
 
 		JLabel lblSelectCategory = new JLabel("Select vehicle category:");
-		add(lblSelectCategory, "flowx,cell 0 4");
+		add(lblSelectCategory, "flowx,cell 0 1");
 
-		//TODO add option: all categories
+		// TODO add option: all categories
 		cbVehicleCategory = new JComboBox<PricelistEntry.VehicleCategory>(PricelistEntry.VehicleCategory.values());
-		cbVehicleCategory.setSelectedItem(null);
-		cbVehicleCategory.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				fillFields();
-			}
-		});
-		add(cbVehicleCategory, "cell 0 4,growx");
+		add(cbVehicleCategory, "cell 0 1,growx");
 
 		JLabel lblCurrency = new JLabel("Select currency:");
-		add(lblCurrency, "flowx,cell 0 5");
+		add(lblCurrency, "flowx,cell 0 2");
 
 		cbCurrency = new JComboBox<PricelistEntry.Currency>(PricelistEntry.Currency.values());
-		cbCurrency.setSelectedItem(null);
-		cbCurrency.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				fillFields();
-			}
-		});
-		add(cbCurrency, "cell 0 5,growx");
+		cbCurrency.setSelectedItem(PricelistEntry.Currency.RSD);
+		add(cbCurrency, "cell 0 2,growx");
+
+		JLabel lblStartDate = new JLabel("From date (format dd.MM.yyyy):");
+		add(lblStartDate, "flowx,cell 0 3");
+		txtStartDate = new JTextField();
+		add(txtStartDate, "cell 0 3,growx");
+
+		JLabel lblEndDate = new JLabel("To date (format dd.MM.yyyy):");
+		add(lblEndDate, "flowx,cell 0 4");
+		txtEndDate = new JTextField();
+		add(txtEndDate, "cell 0 4,growx");
 
 		JLabel lblProfit = new JLabel("Total profit:");
-		add(lblProfit, "flowx,cell 0 6");
+		add(lblProfit, "flowx,cell 0 5");
 
 		txtProfit = new JTextField();
 		txtProfit.setEnabled(false);
-		add(txtProfit, "cell 0 6,growx");
+		add(txtProfit, "cell 0 5,growx");
 		txtProfit.setColumns(10);
 
-		JLabel lblChange = new JLabel("Change:");
-		add(lblChange, "flowx,cell 0 8");
-		
 		btnConfirm = new JButton("Confirm");
 		btnConfirm.setEnabled(false);
 		btnConfirm.addActionListener(new ActionListener() {
@@ -103,61 +110,99 @@ public class ReportDashboardView extends JPanel implements ITabbedPanel {
 				generateReport();
 			}
 		});
-		add(btnConfirm, "flowx,cell 0 9");
+		add(btnConfirm, "flowx,cell 0 6");
+		btnConfirm.setEnabled(true);
 	}
 
 	@Override
 	public void onShow() {
 
 	}
-	
-	private void checkIfCanProcess() {
-		if (Integer.decode(txtProfit.getText()) >= 0) {
-			btnConfirm.setEnabled(true);
-		} else {
-			btnConfirm.setEnabled(false);
-		}
-	}
 
-	private void resetFields() {
-		txtStartDate.setText("");
-		txtEndDate.setText("");
-		txtProfit.setText("");
-		cbVehicleCategory.setSelectedItem(null);
-		cbCurrency.setSelectedItem(null);
-		btnConfirm.setEnabled(false);
-	}
+	/*
+	 * private void resetFields() { txtStartDate.setText("");
+	 * txtEndDate.setText(""); txtProfit.setText("");
+	 * cbVehicleCategory.setSelectedItem(null); cbCurrency.setSelectedItem(null);
+	 * btnConfirm.setEnabled(false); }
+	 */
 
-	private void fillFields() {
+	private void validateFields() throws ParseException, FieldEmptyException, DateTimeException {
 		if (cbStation.getSelectedIndex() == -1 || cbCurrency.getSelectedIndex() == -1
 				|| cbVehicleCategory.getSelectedIndex() == -1) {
-			return;
+			throw new FieldEmptyException();
 		}
 
-		getEntry();
+		Date tempStartDate;
+		Date tempEndDate;
+		try {
+			SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+			tempStartDate = formatter.parse(txtStartDate.getText().trim());
+			tempEndDate = formatter.parse(txtEndDate.getText().trim());
+		} catch (ParseException e) {
+			throw e;
+		}
 
-		txtProfit.setText(String.valueOf("100")); 	//TODO setProfit with corresponding value
-
-		checkIfCanProcess();
-	}
-
-	private void getEntry() {
-		Station station = null; 		//TODO get selected station
-		PricelistEntry.VehicleCategory category = (PricelistEntry.VehicleCategory) cbVehicleCategory.getSelectedItem();
-		PricelistEntry.Currency currency = (PricelistEntry.Currency) cbCurrency.getSelectedItem();
-		//TODO get rest of the data
+		if (tempStartDate.after(tempEndDate)) {
+			throw new DateTimeException("Invalid date interval");
+		}
 		
-		//this.inputStation = station;
-		//this.inputCategory = category;
-		//this.inputCurrency = currency;
-		//TODO set rest of the data
+		this.startDate = tempStartDate;
+		this.endDate = tempEndDate;
 	}
 
 	private void generateReport() {
-		
-		//TODO: create a chart instance and a thread to run it
-		
-		resetFields();
-	}
+		try {
+			validateFields();
+		} catch (ParseException e) {
+			JOptionPane.showMessageDialog(null, "Incorrect date format. Must be dd.MM.yyyy", "Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		} catch (FieldEmptyException e) {
+			JOptionPane.showMessageDialog(null, "Field cannot be empty.", "Error", JOptionPane.ERROR_MESSAGE);
+			return;
+		} catch (DateTimeException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.WARNING_MESSAGE);
+			return;
+		}
 
+		getDataForReport();
+	}
+	
+	private void getDataForReport() {
+		List<Station> stations;
+		List<PricelistEntry.VehicleCategory> categories;
+		
+		if (cbStation.getSelectedItem().toString().equals("ALL")) {
+			stations = ctx.getStationService().getByType(Type.EXIT);
+		}
+		else {
+			stations = new ArrayList<Station>();
+			stations.add((Station) cbStation.getSelectedItem());
+		}
+		if (cbVehicleCategory.getSelectedItem().toString().equals("ALL")) {
+			categories = Arrays.asList(PricelistEntry.VehicleCategory.values());
+		}
+		else {
+			categories = new ArrayList<PricelistEntry.VehicleCategory>();
+			categories.add((PricelistEntry.VehicleCategory) cbVehicleCategory.getSelectedItem());
+		}
+		PricelistEntry.Currency currency = (PricelistEntry.Currency) cbCurrency.getSelectedItem();
+		txtProfit.setText(String.valueOf("100")); // TODO setProfit with corresponding value
+		
+		ReportXChart report;
+		try {
+			report = new ReportXChart(stations, categories, currency, this.startDate, this.endDate, ctx);
+			
+			Thread t = new Thread(new Runnable() {
+				@SuppressWarnings({ "unchecked", "rawtypes" })
+				@Override
+				public void run() {
+					new SwingWrapper(report.chart).displayChart()
+							.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+				}
+			});
+			t.start();
+		} catch (EmptyAxisException e) {
+			JOptionPane.showMessageDialog(null, "There is no data to show", "Error", JOptionPane.WARNING_MESSAGE);
+		}
+	}
 }
---
+++ src/desktop/report/ReportXChart.java
@@ -0,0 +1,67 @@
+package desktop.report;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import core.AppContext;
+import core.pricelist.entry.PricelistEntry.Currency;
+import core.pricelist.entry.PricelistEntry.VehicleCategory;
+import core.station.Station;
+
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.style.colors.ChartColor;
+import org.knowm.xchart.style.colors.XChartSeriesColors;
+import org.knowm.xchart.style.lines.SeriesLines;
+import org.knowm.xchart.style.markers.SeriesMarkers;
+
+public class ReportXChart {
+
+	private AppContext ctx;
+	private List<Date> xAxis; //dates
+	private List<Double> yAxis; //earnings
+	public XYChart chart;
+
+	@SuppressWarnings("deprecation")
+	public ReportXChart(List<Station> stations, List<VehicleCategory> categories, Currency currency, Date startDate,
+			Date endDate, AppContext ctx) throws EmptyAxisException {
+		this.ctx = ctx;
+		this.yAxis = new ArrayList<Double>();
+		this.xAxis = new ArrayList<Date>();
+		
+		// ukoliko je enddate - startdate == 1 onda ispisuj sate...
+		// odvojiti u posebne funkcije... buvalno ovdje samo dobaviti payment za taj dan
+		
+		for(Date currDate=startDate; currDate.before(endDate); currDate.setDate(currDate.getDate()+1)) {
+			double profit = this.ctx.getPaymentService().getProfitForReport(stations, categories, currency, currDate);
+			if (profit == 0) continue;
+			yAxis.add(profit);
+			xAxis.add(currDate);
+			System.out.println(profit);
+			System.out.println(currDate);
+		}
+		
+		if (xAxis.isEmpty() || yAxis.isEmpty()) {
+			throw new EmptyAxisException();
+		}
+		
+		chart = new XYChartBuilder().xAxisTitle("Date").yAxisTitle("Earnings").width(600).height(400).build();
+		
+	    chart.getStyler().setPlotBackgroundColor(ChartColor.getAWTColor(ChartColor.GREY));
+	    chart.getStyler().setPlotGridLinesColor(new Color(255, 255, 255));
+	    chart.getStyler().setChartBackgroundColor(Color.WHITE);
+	    chart.getStyler().setPlotGridLinesVisible(false);
+	    chart.getStyler().setDatePattern("dd-MMM");
+	    
+		XYSeries series = chart.addSeries("Earnings by date", xAxis, yAxis);
+		series.setMarker(SeriesMarkers.NONE);
+		series.setLineColor(XChartSeriesColors.BLUE);
+	    series.setMarkerColor(Color.ORANGE);
+	    series.setMarker(SeriesMarkers.CIRCLE);
+	    series.setLineStyle(SeriesLines.SOLID);
+		chart.getStyler().setDatePattern("dd-MMM");
+	}
+}
