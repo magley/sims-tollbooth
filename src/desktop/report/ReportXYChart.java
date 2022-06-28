e3c2e0199c2a936326904cba63a582662c00b3bb
Hristina Adamovic <hristinacina@gmail.com>
2022-06-28 20:34:34 +0200

    Renamed

--- src/desktop/report/ReportXYChart.java
+++ src/desktop/report/ReportXYChart.java
@@ -18,52 +18,50 @@ import org.knowm.xchart.style.colors.XChartSeriesColors;
 import org.knowm.xchart.style.lines.SeriesLines;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
-public class ReportXChart {
+public class ReportXYChart {
 
 	private AppContext ctx;
 	private List<Integer> xAxis; //dates
-	private List<Double> yAxis; //earnings
+	private List<Integer> yAxis; //earnings
 	public XYChart chart;
+	public int totalProfit;
 
 	@SuppressWarnings("deprecation")
-	public ReportXChart(List<Station> stations, List<VehicleCategory> categories, Currency currency, Date startDate,
+	public ReportXYChart(List<Station> stations, List<VehicleCategory> categories, Currency currency, Date startDate,
 			Date endDate, AppContext ctx) throws EmptyAxisException {
 		this.ctx = ctx;
-		this.yAxis = new ArrayList<Double>();
+		this.yAxis = new ArrayList<Integer>();
 		this.xAxis = new ArrayList<Integer>();
 		
-		// ukoliko je enddate - startdate == 1 onda ispisuj sate...
-		// odvojiti u posebne funkcije... buvalno ovdje samo dobaviti payment za taj dan
-		
-		int i = 0;
+		Integer i = 0;
 		for(Date currDate=startDate; currDate.before(endDate); currDate.setDate(currDate.getDate()+1)) {
-			double profit = this.ctx.getPaymentService().getProfitForReport(stations, categories, currency, currDate);
-			//if (profit == 0) continue;
+			Integer profit = this.ctx.getPaymentService().getProfitForReport(stations, categories, currency, currDate);
 			yAxis.add(profit);
 			xAxis.add(i);
+			totalProfit+=profit;
 			i++;
-			System.out.println(profit);
-			System.out.println(currDate);
 		}
 		
 		if (xAxis.isEmpty() || yAxis.isEmpty()) {
 			throw new EmptyAxisException();
 		}
 		
-		chart = new XYChartBuilder().xAxisTitle("Date").yAxisTitle("Earnings").width(600).height(400).build();
+		initChart();
+	}
+	
+	private void initChart() {
+		chart = new XYChartBuilder().xAxisTitle("Serial day number").yAxisTitle("Earnings").width(700).height(500).build();
 		
 	    chart.getStyler().setPlotBackgroundColor(ChartColor.getAWTColor(ChartColor.GREY));
 	    chart.getStyler().setPlotGridLinesColor(new Color(255, 255, 255));
 	    chart.getStyler().setChartBackgroundColor(Color.WHITE);
 	    chart.getStyler().setPlotGridLinesVisible(false);
-	    chart.getStyler().setDatePattern("dd-MMM");
 	    
-		XYSeries series = chart.addSeries("Earnings by date", xAxis, yAxis);
+		XYSeries series = chart.addSeries("Earnings by day", xAxis, yAxis);
 		series.setMarker(SeriesMarkers.NONE);
 		series.setLineColor(XChartSeriesColors.BLUE);
 	    series.setMarkerColor(Color.ORANGE);
 	    series.setMarker(SeriesMarkers.CIRCLE);
 	    series.setLineStyle(SeriesLines.SOLID);
-		chart.getStyler().setDatePattern("dd-MMM");
 	}
 }
